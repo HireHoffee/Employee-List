ac2cf355b49a9efda8bd77f118263474d1b67027
Rostislav Alekseev <mr.rostik.01@gmail.com>
2026-01-28 00:52:20 +0200

    вёрстка окна сортировки

--- pages/home/ui/HomePage.tsx
+++ pages/home/ui/HomePage.tsx
@@ -1,10 +1,11 @@
 import EmployeeCard from "@/shared/ui/EmployeeCard";
+import SortDrawer from "@/shared/ui/SortDrawer";
 import TopBar from "@/widgets/topbar/ui/TopBar";
 import { FlatList, StyleSheet, View } from "react-native";
 
 const HomePage = () => {
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, position: "relative" }}>
       <TopBar />
       <FlatList
         data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]}
@@ -13,6 +14,7 @@ const HomePage = () => {
         contentContainerStyle={styles.cardsContainer}
         showsVerticalScrollIndicator={false}
       />
+      <SortDrawer />
     </View>
   );
 };
---
+++ shared/ui/SortDrawer.tsx
@@ -0,0 +1,64 @@
+import SelectedIcon from "@/shared/assets/svgs/selected-icon.svg";
+import UnselectedIcon from "@/shared/assets/svgs/unselected-icon.svg";
+import React from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+const SortDrawer = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.drawer}>
+        <View style={styles.draggableLine}></View>
+        <Text style={{ fontWeight: "bold", textAlign: "center", fontSize: 24 }}>Сортировка</Text>
+        <View style={styles.options}>
+          <Pressable style={styles.option}>
+            <SelectedIcon />
+            <Text style={{ fontSize: 16 }}>По алфавиту</Text>
+          </Pressable>
+          <Pressable style={styles.option}>
+            <UnselectedIcon />
+            <Text style={{ fontSize: 16 }}>По дню рождения</Text>
+          </Pressable>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: "none", // временно
+    position: "absolute",
+    height: "100%",
+    width: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.2)",
+    paddingHorizontal: 8,
+    justifyContent: "flex-end",
+  },
+  draggableLine: {
+    width: 56,
+    height: 4,
+    backgroundColor: "#c3c3c6",
+    borderRadius: 2,
+    marginVertical: 10,
+    alignSelf: "center",
+  },
+  drawer: {
+    height: 220,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    paddingHorizontal: 16,
+  },
+  options: {
+    marginHorizontal: 16,
+    marginVertical: 16,
+    marginTop: 8,
+  },
+  option: {
+    flexDirection: "row",
+    gap: 12,
+    paddingVertical: 18,
+  },
+});
+
+export default SortDrawer;
