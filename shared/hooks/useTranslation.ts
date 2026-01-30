ef82a0218482b9d803651acdd31010621c7eba79
Rostislav Alekseev <mr.rostik.01@gmail.com>
2026-01-30 12:33:29 +0200

    баг фикс

--- shared/hooks/useTranslation.ts
+++ shared/hooks/useTranslation.ts
@@ -2,9 +2,10 @@ import { $locale, changeLanguage } from "@/shared/store/locale";
 import { useUnit } from "effector-react";
 
 export const useTranslation = () => {
-  const locale = useUnit($locale);
+  const [locale, changeTextLanguage] = useUnit([$locale, changeLanguage]);
+
   return {
     locale,
-    changeLanguage,
+    changeLanguage: changeTextLanguage,
   };
 };
