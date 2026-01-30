7f28c72fd4ad0c834f96f2878f7cb042eef906af
Rostislav Alekseev <mr.rostik.01@gmail.com>
2026-01-30 14:48:06 +0200

    лишний console.log

--- app/employee/[id].tsx
+++ app/employee/[id].tsx
@@ -1,15 +1,10 @@
 import EmployeePage from "@/pages/employee/ui/EmployeePage";
 import { useTheme } from "@/shared/hooks/useTheme";
 import { basicStyles } from "@/shared/utils/basicStyles";
-import { useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Employee() {
   const { theme, dark, light } = useTheme();
-
-  useEffect(() => {
-    console.log(theme);
-  }, [theme]);
 
   return (
     <SafeAreaView
